6796c794533f1978b3022867fe9851d8033c7ea7
Arkeopix <mendiej750@gmail.com>
2015-06-16 15:41:21 +0200

    problem6 done

--- euler.lisp
+++ euler.lisp
@@ -146,3 +146,6 @@
 (defun problem6 ()
   (sum-square-difference 100))
 
+
+;; By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+;; What is the 10001st prime number?
