b17d8ff874ac50837a75abbec815b5de9ea07c6c
Arkeopix <mendiej750@gmail.com>
2015-06-16 10:16:00 +0200

    problem5 done

--- euler.lisp
+++ euler.lisp
@@ -80,9 +80,6 @@
 (defun problem3 (a)
   (get-largest-prime-factor a 2))
 
-
-
-
 ;; A palindromic number reads the same both ways. The largest palindrome made
 ;; from the product of two 2-digit numbers is 9009 = 91 × 99.
 ;; Find the largest palindrome made from the product of two 3-digit numbers.
@@ -107,3 +104,21 @@
   
 (defun problem4 ()
   (get-largest-palindrome-of-2-3-digit-number 999 999 0))
+
+;; 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+;; What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+;; i'm glad i checked, common lisp have a builtin function for gcd/lcm
+;; (defun gcd (a b)
+;;   (loop for tmp from a below)))
+
+;; i loled a little at this solution
+;; (defun problem5 ()
+;;   (lcm 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20))
+
+(defun get-lcm (a b)
+  (cond ((or (= a 0) (= b 0)) 0)
+		(t (abs (/ (* a b) (gcd a b))))))
+
+(defun my-lcm (&rest list)
+  (reduce #'get-lcm list :initial-value 1)) ;; hooray =)
